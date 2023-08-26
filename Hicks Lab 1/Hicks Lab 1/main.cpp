8d3ad6d7026d806565fd92887a22b71bc524da99
Joe Hicks <103856984+BeansAreNeat@users.noreply.github.com>
2023-08-26 15:46:21 -0400

    Add files via upload

--- Hicks Lab 1/Hicks Lab 1/main.cpp
+++ Hicks Lab 1/Hicks Lab 1/main.cpp
@@ -4,37 +4,33 @@
 using namespace std;
 
 int main() {
-	string lines1, lines2; // Number linked to each file
+	string text1, text2;
+	string file1Name = "file1.txt";
+	string file2Name = "file2.txt";
 	int lineCount = 0;
-	ifstream file1("file1.txt");
-	ifstream file2("file2.txt");
+	ifstream file1(file1Name);
+	ifstream file2(file2Name);
 
 	if (file1.is_open() && file2.is_open()) {
-		while (getline(file1, lines1) && getline(file2, lines2)) {
-			lineCount++;
-			cout << "file1.txt: " << lines1;
-
-			// Compares lengths of text
-			size_t minLength;
-			if (lines1.length() < lines2.length())
-				minLength = lines1.length();
-			else
-				minLength = lines2.length();
-
-			for (size_t i = 0; i < minLength; ++i) {
-				if (lines1[i] != lines2[i]) {
-					cout << "file1.txt: " << lineCount << ": " << lines1 << endl;
-					cout << "file2.txt: " << lineCount << ": " << lines2 << endl;
-					cout << string(i, ' ') << "^" << endl;
+		// Loops through each line and interates line count
+		while (getline(file1, text1) && getline(file2, text2)) {
+			++lineCount;
+			size_t minLength = min(text1.length(), text2.length());
+			for (int i = 0; i < minLength; i++) {
+				if (text1[i] != text2[i]) {
+					int offSet = file2Name.length() + 2 + lineCount + i;
+					cout << file1Name << ": " << lineCount << ": " << text1 << endl;
+					cout << file2Name << ": " << lineCount << ": " << text2 << endl;
+					cout << string(offSet, ' ') << "^" << endl;
 					break;
 				}
 			}
-			file1.close();
-			file2.close();
 		}
+		file1.close();
+		file2.close();
+	} else {
+		std::cout << "Error";
 	}
-	else {
-		std::cout << "Cry about it";
-	}
+	std::cout << std::endl;
 	return 0;
 }
